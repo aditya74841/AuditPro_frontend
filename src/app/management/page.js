f554611032a8537dbb0950b38af200ff37769edf
Aditya Ranjan <aditya74810@gmail.com>
2025-10-05 00:24:02 +0530

    undated iframe url

--- src/app/management/page.js
+++ src/app/management/page.js
@@ -5,7 +5,7 @@ const IframePage = () => {
   return (
     <div className="w-full h-screen bg-gray-100">
       <iframe
-        src="https://project-management-client-amber.vercel.app/features?projectId=68e128e2bea2411b1ee3d6c9"
+        src="https://pm.allaboutcse.com/features?projectId=68e128e2bea2411b1ee3d6c9"
         className="w-full h-full border-0"
         title="Embedded Project Management App"
       ></iframe>
